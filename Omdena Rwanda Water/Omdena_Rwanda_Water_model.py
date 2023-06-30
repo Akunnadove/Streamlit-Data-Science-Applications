26c5c485d4e9a53ab8b2720b5ff39ff17da82920
Akunna Anyamkpa <66309302+Akunnadove@users.noreply.github.com>
2023-06-30 17:02:26 +0100

    Update Omdena_Rwanda_Water_model.py

--- Omdena Rwanda Water/Omdena_Rwanda_Water_model.py
+++ Omdena Rwanda Water/Omdena_Rwanda_Water_model.py
@@ -1,5 +1,5 @@
 import joblib
  
 def predict(data):
-    lr = joblib.load('https://github.com/Akunnadove/Streamlit-Data-Science-Applications/blob/main/Omdena%20Rwanda%20Water/Omdena_Rwanda_Water_model.joblib')
+    lr = joblib.load('Omdena_Rwanda_Water_model.joblib')
     return lr.predict(data) 
